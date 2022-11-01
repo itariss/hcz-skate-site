cb136ab18edc26f78aedc37e3df91ebca750931c
itariss <itariss@protonmail.com>
2022-11-01 17:22:01 -0300

    Styling Cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,6 @@
 	const menuLateral = document.querySelector(".menu-lateral");
 
 	menuBtn.addEventListener("click", () => {
-		console.log("oi");
 		menuLateral.classList.toggle("menu-lateral--ativo");
 	});
 })();
